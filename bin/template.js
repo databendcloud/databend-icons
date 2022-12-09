01ef8b7b1333b876f427ab9f7d2d0c751370ebf2
Chasen-Zhang <787025321@qq.com>
2022-12-09 14:23:39 +0800

    fix:

--- bin/template.js
+++ bin/template.js
@@ -26,7 +26,7 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
   import clsx from 'clsx';
 
   const ${ComponentName} = (props) => {
-    const { color, size, ...otherProps } = props;
+    const { color, size, className, ...otherProps } = props;
     return (
       <svg className={clsx('icon default', className)} ${attrs}>
         ${svgCode}
