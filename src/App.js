d7ee0ba40893777d198d053155c4774ec14996f1
Chasen-Zhang <787025321@qq.com>
2022-12-09 22:18:52 +0800

    fix: copy svg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import IconWrapper from './components/IconWrapper'
 import { copyToClipboard } from 'copyforjs';
 import { Tooltip, Space, message } from 'antd';
 
-
 const Container = styled.ul`
   display: grid;
   grid-template-columns: repeat(8, 1fr);
@@ -14,11 +13,15 @@ const Container = styled.ul`
   column-gap: 20px;
   align-items: stretch;
   margin: 0;
-  padding: 0 0 50px 20px;
+  padding: 10px 20px 50px 20px;
   list-style: none;
 `
-
+function msg() {
+  message.destroy();
+  message.success('已复制')
+} 
 class List extends React.Component {
+  
   render() {
     return (
       <div>
@@ -40,14 +43,20 @@ class List extends React.Component {
                   <IconWrapper>
                     <Space size={6} align="center" direction="vertical">
                       <Tooltip title="点击复制svg">
-                        <Icon size={30} onClick={()=> console.log(Icon, 'Icon')}/>
+                        <Icon id={key} size={30} onClick={(e)=> {
+                          const svgInner = document?.getElementById(key)?.innerHTML;
+                          const wrap = 
+                          `<svg class="icon default" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">${svgInner}<svg>
+                          `
+                          copyToClipboard(wrap);
+                          msg();
+                        }}/>
                       </Tooltip>
                       <Space size={4}>
                         <span>{key}</span>
                         <span onClick={() => {
-                          message.destroy();
-                          message.success('已复制')
-                          copyToClipboard(key)
+                          copyToClipboard(key);
+                          msg();
                         }}>
                           <Tooltip title="点击复制名称">
                             <Copy></Copy>
